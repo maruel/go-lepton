a0624d77c84330c0c97916363a305698933c4068
Marc-Antoine Ruel <maruel@chromium.org>
2015-02-10 22:11:27 -0500

    Create go-lepton.
    It's a small project to drive a FLIR Lepton on a Raspberry Pi in Golang.

---
+++ README.md
@@ -0,0 +1,50 @@
+go-lepton
+=========
+
+Serves images taken on a FLIR Lepton connected to a Raspberry Pi SPI port to
+over HTTP.
+
+To be able to use the SPI port on the Raspberry Pi without root, run the
+following:
+
+    sudo groupadd -f --system spi
+    sudo adduser $USER spi
+    echo 'SUBSYSTEM=="spidev", GROUP="spi"' | sudo tee --append /etc/udev/rules.d/90-spi.rules > /dev/null
+
+Then reboot. Then connect the port as shown at
+https://github.com/PureEngineering/LeptonModule/wiki
+
+
+Performance
+-----------
+
+Reading the SPI port currently saturate the CPU of a Raspberry Pi v1.
+
+    (pprof) top20
+    21.23s of 24.38s total (87.08%)
+    Dropped 71 nodes (cum <= 0.12s)
+    Showing top 20 nodes out of 36 (cum >= 0.50s)
+         flat  flat%   sum%        cum   cum%
+       12.67s 51.97% 51.97%     14.28s 58.57%  syscall.Syscall
+        1.79s  7.34% 59.31%      2.17s  8.90%  runtime.mallocgc
+        0.96s  3.94% 63.25%     19.21s 78.79%  github.com/kidoman/embd/host/generic.(*spiBus).TransferAndRecieveData
+        0.92s  3.77% 67.02%      0.92s  3.77%  runtime.usleep
+        0.82s  3.36% 70.39%     20.03s 82.16%  main.(*Lepton).readLine
+        0.46s  1.89% 72.27%      3.04s 12.47%  runtime.convT2E
+        0.43s  1.76% 74.04%      0.43s  1.76%  main.(*rawBuffer).eq
+        0.33s  1.35% 75.39%      0.99s  4.06%  main.(*rawBuffer).scale
+        0.31s  1.27% 76.66%      0.38s  1.56%  runtime.casgstatus
+        0.29s  1.19% 77.85%      0.29s  1.19%  udiv
+        0.26s  1.07% 78.92%      0.77s  3.16%  runtime.reentersyscall
+        0.26s  1.07% 79.98%      0.26s  1.07%  scanblock
+        0.25s  1.03% 81.01%      0.25s  1.03%  ExternalCode
+        0.25s  1.03% 82.03%      0.25s  1.03%  runtime.memclr
+        0.23s  0.94% 82.98%      0.72s  2.95%  runtime.exitsyscall
+        0.23s  0.94% 83.92%      0.23s  0.94%  runtime.memmove
+        0.21s  0.86% 84.78%      0.30s  1.23%  exitsyscallfast
+        0.20s  0.82% 85.60%      0.21s  0.86%  runtime.cas
+        0.19s  0.78% 86.38%      0.19s  0.78%  runtime.MSpan_Sweep
+        0.17s   0.7% 87.08%      0.50s  2.05%  github.com/golang/glog.V
+
+Not sure how to improve the situation but this leaves absolutely no room for
+any computation.
---
+++ main.go
@@ -0,0 +1,298 @@
+// Copyright 2015 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+// Takes video from FLIR Lepton connected to a Raspberry Pi SPI port.
+//
+// References:
+// http://www.pureengineering.com/projects/lepton
+//
+// FLIR LEPTON® Long Wave Infrared (LWIR) Datasheet
+//   https://drive.google.com/file/d/0B3wmCw6bdPqFblZsZ3l4SXM4R28/view
+//   p. 7 Sensitivity is below 0.05°C
+//   p. 19-21 Telemetry mode; TODO(maruel): Enable.
+//   p. 22-24 Radiometry mode; TODO(maruel): Enable.
+//   p. 28-35 SPI protocol explanation.
+//
+// Help group mailing list:
+//   https://groups.google.com/d/forum/flir-lepton
+//
+// Connecting to a Raspberry Pi:
+//   https://github.com/PureEngineering/LeptonModule/wiki
+//
+// Lepton™ Software Interface Description Document (IDD):
+//   (Application level and SDK doc)
+//   https://drive.google.com/file/d/0B3wmCw6bdPqFOHlQbExFbWlXS0k/view
+//   p. 36-37 Ping and Status, implement first to ensure i²c works.
+//   p. 42-43 Telemetry enable.
+//   Radiometry is not documented (!?)
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	"image/png"
+	"net/http"
+	"os"
+	"runtime/pprof"
+	"sync"
+	"time"
+
+	"github.com/kidoman/embd"
+	_ "github.com/kidoman/embd/host/all"
+	"github.com/maruel/interrupt"
+)
+
+type rawBuffer [60][80]uint16
+
+// scale reduces the dynamic range of a 14 bits down to 8 bits very naively.
+func (r *rawBuffer) scale(dst *image.Gray) {
+	floor := r.minVal()
+	delta := int(r.maxVal() - floor)
+	for y := 0; y < 60; y++ {
+		for x := 0; x < 80; x++ {
+			v := (int(r[y][x] - floor)) * 255 / delta
+			dst.Pix[dst.Stride*y+x] = uint8(v)
+		}
+	}
+}
+
+func (r *rawBuffer) maxVal() uint16 {
+	out := uint16(0)
+	for y := 0; y < 60; y++ {
+		for x := 0; x < 80; x++ {
+			if r[y][x] > out {
+				out = r[y][x]
+			}
+		}
+	}
+	return out
+}
+
+func (r *rawBuffer) minVal() uint16 {
+	out := uint16(0xffff)
+	for y := 0; y < 60; y++ {
+		for x := 0; x < 80; x++ {
+			if r[y][x] < out {
+				out = r[y][x]
+			}
+		}
+	}
+	return out
+}
+
+func (r *rawBuffer) eq(l *rawBuffer) bool {
+	for y := 0; y < 60; y++ {
+		for x := 0; x < 80; x++ {
+			if r[y][x] != l[y][x] {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+type rawBufferRing struct {
+	c chan *rawBuffer
+}
+
+func makeRawBufferRing() *rawBufferRing {
+	return &rawBufferRing{c: make(chan *rawBuffer, 16)}
+}
+
+func (r *rawBufferRing) get() *rawBuffer {
+	select {
+	case b := <-r.c:
+		return b
+	default:
+		return &rawBuffer{}
+	}
+}
+
+func (r *rawBufferRing) done(b *rawBuffer) {
+	if len(r.c) < 8 {
+		r.c <- b
+	}
+}
+
+type Lepton struct {
+	spiBus          embd.SPIBus
+	currentImg      *rawBuffer
+	currentLine     int
+	packet          [164]uint8 // one line is sent as a SPI packet.
+	goodFrames      int        // stats
+	duplicateFrames int
+	brokenPackets   int
+	syncFailures    int
+	dummyLines      int
+}
+
+func MakeLepton(spiBus embd.SPIBus) *Lepton {
+	return &Lepton{spiBus: spiBus, currentLine: -1}
+}
+
+// readLine reads one line at a time.
+//
+// Each line is sent as a packet over SPI. The packet size is constant. See
+// page 28-35 for SPI protocol explanation.
+// https://drive.google.com/file/d/0B3wmCw6bdPqFblZsZ3l4SXM4R28/view
+func (l *Lepton) readLine() {
+	// Operation must complete within 32ms. Frames occur every 38.4ms. With SPI,
+	// write must occur as read is being done, just sent dummy data.
+	if err := l.spiBus.TransferAndRecieveData(l.packet[:]); err != nil {
+		l.brokenPackets++
+		l.currentLine = -1
+		return
+	}
+
+	if (l.packet[0] & 0xf) == 0x0f {
+		// Discard packet. This happens as the bandwidth of SPI is larger than data
+		// rate.
+		l.currentLine = -1
+		l.dummyLines++
+		return
+	}
+
+	// If out of sync, Deassert /CS and idle SCK for at least 5 frame periods
+	// (>185ms).
+
+	// TODO(maruel): Verify CRC (bytes 2-3) ?
+	line := int(l.packet[1])
+	if line > 60 {
+		time.Sleep(200 * time.Millisecond)
+		l.brokenPackets++
+		l.currentLine = -1
+		return
+	}
+	if line != l.currentLine+1 {
+		time.Sleep(200 * time.Millisecond)
+		l.syncFailures++
+		l.currentLine = -1
+		return
+	}
+
+	// Convert the line from byte to uint16. 14 bits significant.
+	l.currentLine++
+	for i := 0; i < 80; i++ {
+		l.currentImg[line][i] = (uint16(l.packet[2*i+4])<<8 | uint16(l.packet[2*i+5]))
+	}
+}
+
+// ReadImg reads an image into currentImg.
+func (l *Lepton) ReadImg(r *rawBuffer) {
+	l.currentLine = -1
+	prevImg := l.currentImg
+	l.currentImg = r
+	for {
+		// TODO(maruel): Fail after N errors?
+		// TODO(maruel): Skip 2 frames since they'll be the same data so no need
+		// for the check below.
+		for l.currentLine != 59 {
+			l.readLine()
+		}
+		if prevImg == nil || !prevImg.eq(l.currentImg) {
+			l.goodFrames++
+			break
+		}
+		// It also happen if the image is static.
+		l.duplicateFrames++
+		l.currentLine = -1
+	}
+}
+
+type doubleBuffer struct {
+	lock        sync.Mutex
+	frontBuffer *image.Gray
+	backBuffer  *image.Gray
+}
+
+var currentImage doubleBuffer
+
+func serveImg(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "image/png")
+	currentImage.lock.Lock()
+	png.Encode(w, currentImage.frontBuffer)
+	currentImage.lock.Unlock()
+}
+
+func mainImpl() error {
+	cpuprofile := flag.String("cpuprofile", "", "dump CPU profile in file")
+	port := flag.Int("port", 8010, "http port to listen on")
+	flag.Parse()
+
+	if len(flag.Args()) != 0 {
+		return fmt.Errorf("unexpected argument: %s", flag.Args())
+	}
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			return err
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	interrupt.HandleCtrlC()
+
+	if err := embd.InitSPI(); err != nil {
+		return err
+	}
+	defer embd.CloseSPI()
+
+	// Max rate is 20Mhz. Minimum usable rate is ~4Mhz to sustain framerate.
+	// Low rate is less likely to get electromagnetic interference and reduces
+	// unnecessary CPU consumption by reducing the number of dummy packets.
+	spiBus := embd.NewSPIBus(embd.SPIMode3, 0, 4000000, 8, 0)
+	defer spiBus.Close()
+
+	l := MakeLepton(spiBus)
+
+	c := make(chan *rawBuffer, 16)
+
+	currentImage.frontBuffer = image.NewGray(image.Rect(0, 0, 80, 60))
+	currentImage.backBuffer = image.NewGray(image.Rect(0, 0, 80, 60))
+	ring := makeRawBufferRing()
+
+	go func() {
+		for {
+			// The idea is to keep this loop busy to not lose sync on SPI.
+			b := ring.get()
+			l.ReadImg(b)
+			c <- b
+		}
+	}()
+
+	go func() {
+		for {
+			// Processing is done in a separate loop.
+			img := <-c
+			img.scale(currentImage.backBuffer)
+			ring.done(img)
+			currentImage.lock.Lock()
+			currentImage.backBuffer, currentImage.frontBuffer = currentImage.frontBuffer, currentImage.backBuffer
+			currentImage.lock.Unlock()
+		}
+	}()
+
+	http.HandleFunc("/", serveImg)
+	fmt.Printf("Listening on %d\n\n", *port)
+	go http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
+
+	for !interrupt.IsSet() {
+		// TODO(maruel): load variables via atomic.
+		fmt.Printf("%d frames %d duped %d badsync %d broken %d dummy\r", l.goodFrames, l.duplicateFrames, l.syncFailures, l.brokenPackets, l.dummyLines)
+		time.Sleep(time.Second)
+	}
+	fmt.Print("\n")
+	return nil
+}
+
+func main() {
+	if err := mainImpl(); err != nil {
+		fmt.Fprintf(os.Stderr, "go-lepton: %s.\n", err)
+		os.Exit(1)
+	}
+}
