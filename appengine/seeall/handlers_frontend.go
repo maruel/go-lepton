0ca3806088a3d800e8f82d10f4fd2a7269e5f67f
Marc-Antoine Ruel <maruel@chromium.org>
2015-02-14 17:15:39 -0500

    Add general stuff for server, not working.

--- appengine/seeall/handlers_frontend.go
+++ appengine/seeall/handlers_frontend.go
@@ -6,32 +6,70 @@ package seeall
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"appengine"
-	"appengine/user"
+	"appengine/datastore"
+	//"appengine/user"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/restricted/sources", sources)
+	//http.HandleFunc("/restricted/sources/add", sourcesAdd)
+	//http.HandleFunc("/restricted/source/", source)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
 }
 
-func handler2(w http.ResponseWriter, r *http.Request) {
+var sourcesTmpl = template.Must(template.New("sources").Parse(`
+<html>
+  <head>
+    <title>See All Sources</title>
+  </head>
+  <body>
+		<h1>Sources</h1>
+		<ul>
+    {{range .Sources}}
+			<li>
+				{{.Who}} - {{.Created}} - {{.Name}} - {{.Details}} - {{.SecretKey}} - {{.IP}}
+				<form action="/restricted/source/TODO_ID/delete" method="POST">
+					<input type="submit" value="Delete">
+				</form>
+			</li>
+    {{end}}
+		</ul>
+  </body>
+</html>
+`))
+
+func sources(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
-	u := user.Current(c)
-	if u == nil {
-		url, err := user.LoginURL(c, r.URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Location", url)
-		w.WriteHeader(http.StatusFound)
+
+	/* POST
+	if u := user.Current(c); u != nil {
+		// = u.String()
+	}
+	key := datastore.NewIncompleteKey(c, "Source", nil)
+	_, err := datastore.Put(c, key, &g)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	fmt.Fprintf(w, "Hello, %v!", u)
+	*/
+
+	// GET
+	q := datastore.NewQuery("Source").Order("-__key__")
+	data := struct {
+		Sources []Source
+	}{}
+	if _, err := q.GetAll(c, &data.Sources); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if err := sourcesTmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
---
+++ appengine/seeall/models.go
@@ -0,0 +1,25 @@
+// Copyright 2015 Marc-Antoine Ruel. All rights reserved.
+// Use of this source code is governed under the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package seeall
+
+import (
+	"time"
+)
+
+// Key is an id starting a 1.
+type Source struct {
+	Who       string    `datastore:""`
+	Created   time.Time `datastore:""`
+	Name      string    `datastore:""`
+	Details   string    `datastore:",noindex"`
+	SecretKey string    `datastore:",noindex"`
+	IP        []string  `datastore:",noindex"`
+}
+
+type Image struct {
+	Who     string    `datastore:""`
+	Created time.Time `datastore:""`
+	PNG     []byte    `datastore:"noindex"`
+}
