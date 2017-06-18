f5349b89c137736d03bc80725c0acab8dc20ee0a
Marc-Antoine Ruel <maruel@chromium.org>
2017-06-18 18:05:47 -0400

    Remove appengine/seeall
    It's just too costly for what is worth. At this point it is cheaper to run on
    f1-micro instance behind a caddy instance.
    
    Remove run.sh since it's not really useful.

--- appengine/run_local.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Copyright 2015 Marc-Antoine Ruel. All rights reserved.
-# Use of this source code is governed under the Apache License, Version 2.0
-# that can be found in the LICENSE file.
-
-BASEDIR="$(dirname $0)"
-
-~/src/go_appengine/goapp serve -host 0.0.0.0 $BASEDIR/seeall
